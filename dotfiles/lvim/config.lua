7bb81927bfc5b778974a7ba12a4bd8960b6572cf
JWATKDK <bryantjohnnyc@johndeere.com>
2023-05-04 09:54:51 -0500

    Add formatters to lvim

--- dotfiles/lvim/config.lua
+++ dotfiles/lvim/config.lua
@@ -129,21 +129,24 @@ lvim.builtin.treesitter.highlight.enable = true
 --   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 -- end
 
--- -- set a formatter, this will override the language server formatting capabilities (if it exists)
--- local formatters = require "lvim.lsp.null-ls.formatters"
--- formatters.setup {
---   { command = "black", filetypes = { "python" } },
---   { command = "isort", filetypes = { "python" } },
---   {
---     -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
---     command = "prettier",
---     ---@usage arguments to pass to the formatter
---     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
---     extra_args = { "--print-with", "100" },
---     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
---     filetypes = { "typescript", "typescriptreact" },
---   },
--- }
+-- set a formatter, this will override the language server formatting capabilities (if it exists)
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  { command = "black", filetypes = { "python" } },
+  { command = "isort", filetypes = { "python" } },
+  {
+    -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+    command = "prettier",
+    ---@usage arguments to pass to the formatter
+    -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+    extra_args = { "--print-with", "100" },
+    ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+    filetypes = { "typescript", "typescriptreact", "markdown" },
+  },
+}
+
+lvim.format_on_save.enabled = true
+lvim.format_on_save.pattern = { "*.lua", "*.md" }
 
 -- -- set additional linters
 -- local linters = require "lvim.lsp.null-ls.linters"
@@ -164,9 +167,9 @@ lvim.builtin.treesitter.highlight.enable = true
 -- }
 
 lvim.plugins = {
-  { 
+  {
     "catppuccin/nvim",
-    as = "catppuccin",
+    name = "catppuccin",
     config = function()
       require("catppuccin").setup({
         flavour = "mocha",
@@ -179,6 +182,15 @@ lvim.plugins = {
     config = function()
       require("user.metals").config()
     end,
+  },
+  {
+    "npxbr/glow.nvim",
+    name = "glow",
+    ft = { "markdown" },
+    config = function()
+      require('glow').setup()
+    end
+    --build = "yay -S glow"
   },
 }
 
