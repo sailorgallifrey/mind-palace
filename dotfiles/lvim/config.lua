3c776e6d529e786681130704ffc2edf05d3ed00e
JWATKDK <bryantjohnnyc@johndeere.com>
2024-07-23 14:49:45 -0500

    Add some plugins for lunarvim

--- dotfiles/lvim/config.lua
+++ dotfiles/lvim/config.lua
@@ -276,6 +276,57 @@ lvim.plugins = {
     event = "BufRead",
   },
   {
+    "rmagatti/goto-preview",
+    config = function()
+      require('goto-preview').setup {
+        width = 120,             -- Width of the floating window
+        height = 25,             -- Height of the floating window
+        default_mappings = true, -- Bind default mappings
+        debug = false,           -- Print debug information
+        opacity = nil,           -- 0-100 opacity level of the floating window where 100 is fully transparent.
+        post_open_hook = nil     -- A function taking two arguments, a buffer and a window to be ran as a hook.
+        -- You can use "default_mappings = true" setup option
+        -- Or explicitly set keybindings
+        -- vim.cmd("nnoremap gpd <cmd>lua require('goto-preview').goto_preview_definition()<CR>")
+        -- vim.cmd("nnoremap gpi <cmd>lua require('goto-preview').goto_preview_implementation()<CR>")
+        -- vim.cmd("nnoremap gP <cmd>lua require('goto-preview').close_all_win()<CR>")
+      }
+    end
+  },
+  {
+    "klen/nvim-test",
+    config = function()
+      require('nvim-test').setup {
+        run = true,               -- run tests (using for debug)
+        commands_create = true,   -- create commands (TestFile, TestLast, ...)
+        filename_modifier = ":.", -- modify filenames before tests run(:h filename-modifiers)
+        silent = false,           -- less notifications
+        term = "terminal",        -- a terminal to run ("terminal"|"toggleterm")
+        termOpts = {
+          direction = "vertical", -- terminal's direction ("horizontal"|"vertical"|"float")
+          width = 96,             -- terminal's width (for vertical|float)
+          height = 24,            -- terminal's height (for horizontal|float)
+          go_back = false,        -- return focus to original window after executing
+          stopinsert = "auto",    -- exit from insert mode (true|false|"auto")
+          keep_one = true,        -- keep only one terminal for testing
+        },
+        runners = {               -- setup tests runners
+          cs = "nvim-test.runners.dotnet",
+          go = "nvim-test.runners.go-test",
+          haskell = "nvim-test.runners.hspec",
+          javascriptreact = "nvim-test.runners.jest",
+          javascript = "nvim-test.runners.jest",
+          lua = "nvim-test.runners.busted",
+          python = "nvim-test.runners.pytest",
+          ruby = "nvim-test.runners.rspec",
+          rust = "nvim-test.runners.cargo-test",
+          typescript = "nvim-test.runners.jest",
+          typescriptreact = "nvim-test.runners.jest",
+        }
+      }
+    end
+  },
+  {
     "npxbr/glow.nvim",
     ft = { "markdown" }
     -- run = "yay -S glow"
@@ -345,7 +396,11 @@ dap.configurations.scala = {
     }
   }
 }
-
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'LspAttached',
+  once = true,
+  callback = vim.lsp.codelens.refresh,
+})
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- vim.api.nvim_create_autocmd("BufEnter", {
 --   pattern = { "*.json", "*.jsonc" },
